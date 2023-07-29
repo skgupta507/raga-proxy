5fc1a70ba8ec05bd113e021bea8bc0a1fcd099f9
Arya Kiran <arya@projectsegfau.lt>
2023-07-29 23:18:43 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"github.com/gofiber/fiber/v2"
+	"codeberg.org/aryak/paattu-proxy/utils"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName:                 "paattu-proxy",
+		EnableTrustedProxyCheck: true,
+		TrustedProxies:          []string{"0.0.0.0/0"},
+		ProxyHeader:             fiber.HeaderXForwardedFor,
+	})
+
+	app.Get("/:id/:path", func(c *fiber.Ctx) error {
+		utils.ProxyRequest(c, "https://c.saavncdn.com/"+c.Params("id")+"/"+c.Params("path"))
+		return nil
+	})
+	app.Listen(GetPort())
+}
+
+// GetPort returns the port to listen on
+func GetPort() string {
+	port := os.Getenv("PAATTU_PROXY_PORT")
+	if port == "" {
+		port = "3000"
+	}
+	return ":" + port
+}
